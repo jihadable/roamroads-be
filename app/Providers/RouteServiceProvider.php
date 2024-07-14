21051d8eaf3b12ad5261d9ebd787f89e4ec714d4
jihadable <umarjihad141004@gmail.com>
2024-07-14 08:52:43 +0700

    remove private env from vercel.json

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -28,9 +28,16 @@ class RouteServiceProvider extends ServiceProvider
             return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
         });
 
-        $this->routes(function () {
-            Route::middleware('api')
-                ->group(base_path('routes/api.php'));
+        $this->routes(function (){
+            if (env("APP_ENV") == "local"){
+                Route::middleware('api')
+                    ->prefix("api")
+                    ->group(base_path('routes/api.php'));
+            }
+            else if (env("APP_ENV") == "production"){
+                Route::middleware('api')
+                    ->group(base_path('routes/api.php'));
+            }
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
