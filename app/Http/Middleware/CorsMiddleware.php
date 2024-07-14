26c18dd829cf04e7f83fdeef7ecbf7ea1baf2cda
jihadable <umarjihad141004@gmail.com>
2024-07-14 20:47:42 +0700

    update cors again

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -13,10 +13,14 @@ class CorsMiddleware {
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
     public function handle(Request $request, Closure $next): Response {
-        $response = $next($request);
+        if ($request->isMethod('OPTIONS')) {
+            $response = response('OK', 200);
+        } else {
+            $response = $next($request);
+        }
 
         $response->headers->set('Access-Control-Allow-Origin', '*');
-        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
         $response->headers->set('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization');
 
         return $response;
