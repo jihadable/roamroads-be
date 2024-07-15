d4608d08050de8dee74958f3bd4f677a428be85e
jihadable <umarjihad141004@gmail.com>
2024-07-15 13:13:30 +0700

    re-migrate

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,42 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use MongoDB\Laravel\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use MongoDB\Laravel\Eloquent\Model as EloquentModel;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable implements JWTSubject {
-    use HasApiTokens, HasFactory, Notifiable;
+class User extends EloquentModel implements JWTSubject {
+    use HasFactory;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        "phone",
-        'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         "_id", 'password', "created_at", "updated_at"
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'password' => 'hashed',
     ];
 
     /**
